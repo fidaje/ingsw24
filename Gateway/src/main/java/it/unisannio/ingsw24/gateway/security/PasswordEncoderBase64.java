ac536490e743a479fec828ed9e0902481c918b08
francescoiuorio <fracyiuorio@gmail.com>
2024-06-29 11:25:05 +0200

    Renamed PasswordEncoderPlain to PasswordEncoderBase64 Added: encoder Base64

--- Gateway/src/main/java/it/unisannio/ingsw24/gateway/security/PasswordEncoderBase64.java
+++ Gateway/src/main/java/it/unisannio/ingsw24/gateway/security/PasswordEncoderBase64.java
@@ -2,15 +2,17 @@ package it.unisannio.ingsw24.gateway.security;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-public class PasswordEncoderPlain implements PasswordEncoder {
+import java.util.Base64;
+
+public class PasswordEncoderBase64 implements PasswordEncoder {
 
     @Override
     public String encode(CharSequence charSequence){
-        return charSequence.toString();
+        return Base64.getEncoder().encodeToString(charSequence.toString().getBytes());
     }
 
     @Override
     public boolean matches(CharSequence charSequence, String s){
-        return charSequence.toString().equals(s);
+        return encode(charSequence).equals(s);
     }
 }
