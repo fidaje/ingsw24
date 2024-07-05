5d6d43aa6497ede07223bd816e2a9c532e07344e
francescoiuorio <fracyiuorio@gmail.com>
2024-07-05 19:16:31 +0200

    added: removeGuest, addGuest, removeFood, addUnPacked TODO: addPacked

--- Gateway/src/main/resources/static/javascript/pantry.js
+++ Gateway/src/main/resources/static/javascript/pantry.js
@@ -29,6 +29,12 @@ function buildBody() {
                     let content = `<p class="id-dispensa"><i class="fas fa-id-badge"></i> ID Dispensa: ${data.id}</p>`;
                     content += `<p class="proprietario"><i class="fas fa-user"></i> Proprietario: <span>${data.ownerUsername}</span></p>`;
 
+                    // Campo di input e pulsante per aggiungere un nuovo ospite
+                    content += '<div class="add-guest">';
+                    content += '<input type="text" id="newGuestUsername" placeholder="Nuovo ospite username">';
+                    content += `<button onclick="addGuest('${id}')">Aggiungi Ospite</button>`;
+                    content += '</div>';
+
                     // Aggiungere i dettagli degli ospiti
                     content += '<p class="ospiti"><i class="fas fa-users"></i> Ospiti:</p>';
                     content += '<div class="guest-container">';
@@ -42,6 +48,26 @@ function buildBody() {
 
                     // Aggiungere i dettagli degli alimenti
                     content += '<p class="alimenti-title"><i class="fas fa-carrot"></i> Alimenti:</p>';
+        		    content += '<button id="showAddUnPackedForm" onclick="toggleAddUnPackedForm()">Aggiungi Alimento</button>';
+                    content += '<div id="addUnPackedForm" style="display:none;">';
+                    content += '<input type="text" id="newUnPackedName" placeholder="Nome alimento">';
+                    content += '<input type="number" id="newUnPackedQuantity" placeholder="Quantità">';
+                    content += '<label for="newFoodFridge">In frigo</label>';
+                    content += '<input type="checkbox" id="newUnPackedFridge">';
+                    content += `<button onclick="addUnPacked('${id}')">Aggiungi</button>`;
+                    content += '</div>';
+
+                    content += '<button id="showAddPackedForm" onclick="toggleAddPackedForm()">Aggiungi Packed</button>';
+                    content += '<div id="addPackedForm" style="display:none;">';
+                    content += '<input type="number" id="newPackedQuantity" placeholder="Quantità">';
+                    content += '<label for="newPackedFridge">In frigo</label>';
+		    content += '<input type="checkbox" id="newPackedFridge">';
+		    content += '<input type="text" id="newPackedExpire" placeholder="Data Scadenza (yyyy-mm-gg)">';
+		    content += '<div id="my-qr-reader-container" style="margin-top: 20px; display: none;"></div>';
+		    content += '</div>';
+
+		    initializeQrScanner();
+
                     data.fuds.forEach(fud => {
                         content += '<div class="food-item">';
                         content += `<p><i class="fas fa-utensils"></i> <span>Nome:</span> ${fud.name}</p>`;
@@ -52,6 +78,7 @@ function buildBody() {
                         if (fud.brand) content += `<p><i class="fas fa-tag"></i> <span>Marca:</span> ${fud.brand}</p>`;
                         if (fud.category) content += `<p><i class="fas fa-list"></i> <span>Categoria:</span> ${fud.category}</p>`;
                         if (fud.nutritionGrade) content += `<p><i class="fas fa-chart-bar"></i> <span>Grado di nutrizione:</span> ${fud.nutritionGrade}</p>`;
+                        content += `<button class="remove-food-btn" onclick="removeFood('${id}', '${fud.name}')">Rimuovi</button>`;
                         content += '</div>';
                     });
 
@@ -97,4 +124,183 @@ function removeGuest(pantryId, username) {
     console.log(xhttp)
 
     xhttp.send();
+}
+
+// Funzione per aggiungere un ospite
+function addGuest(pantryId) {
+    const newGuestUsername = document.getElementById('newGuestUsername').value;
+    if (!newGuestUsername) {
+        alert("Inserisci un username per il nuovo ospite.");
+        return;
+     }
+
+    const url = "http://" + host + ":8080/ingsw24/gateway/" + pantryId + "/guests";
+    const credentials = sessionStorage.getItem("credentials");
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.withCredentials = true;
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+                console.log(`Guest ${newGuestUsername} added successfully`);
+                // Ricaricare il corpo per aggiornare l'elenco degli ospiti
+                buildBody();
+            } else {
+                console.error('There was a problem with the add operation:', this.statusText);
+            }
+        }
+    };
+
+    xhttp.open("PUT", url, true);
+    xhttp.setRequestHeader("Authorization", "Basic " + credentials);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    
+    xhttp.send(newGuestUsername);
+}
+
+// Funzione per rimuovere un alimento
+function removeFood(pantryId, name) {
+    const url = "http://"+host+":8080/ingsw24/gateway/"+pantryId+"/foods/"+name;
+    const credentials = sessionStorage.getItem("credentials");
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.withCredentials = true;
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            if (this.status === 204) {
+                console.log(`Food ${name} removed successfully`);
+                // Ricaricare il corpo per aggiornare l'elenco degli ospiti
+                buildBody();
+            } else {
+                console.error('There was a problem with the delete operation:', this.statusText);
+            }
+        }
+    };
+
+    xhttp.open("DELETE", url, true);
+    xhttp.setRequestHeader("Authorization", "Basic " + credentials);
+
+    console.log(xhttp)
+
+    xhttp.send();
+}
+
+function toggleAddUnPackedForm() {
+    var form = document.getElementById("addUnPackedForm");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
+}
+
+function addUnPacked(pantryId) {
+    const name = document.getElementById('newUnPackedName').value;
+    const quantity = document.getElementById('newUnPackedQuantity').value;
+    const isFridge = document.getElementById('newUnPackedFridge').checked;
+
+    if (!name || !quantity) {
+        alert("Inserisci nome e quantità dell'alimento.");
+        return;
+    }
+
+    const url = "http://" + host + ":8080/ingsw24/gateway/" + pantryId + "/foods/unpacked/"+name+"?isFridge="+isFridge+"&quantity="+quantity;
+    const credentials = sessionStorage.getItem("credentials");
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.withCredentials = true;
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+                console.log(`Food ${name} added successfully`);
+                // Ricaricare il corpo per aggiornare l'elenco degli alimenti
+                buildBody();
+            } else {
+                console.error('There was a problem with the add operation:', this.statusText);
+            }
+        }
+    };
+
+    xhttp.open("PUT", url, true);
+    xhttp.setRequestHeader("Authorization", "Basic " + credentials);
+
+    xhttp.send();
+}
+
+function toggleAddPackedForm() {
+    var form = document.getElementById("addPackedForm");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
+}
+
+function domReady(fn) {
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        setTimeout(fn, 1000);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+function initializeQrScanner() {
+
+	domReady(function () {
+        	// Crea dinamicamente il div del lettore QR
+        	const qrReaderContainer = document.getElementById('my-qr-reader-container');
+        	const qrReaderDiv = document.createElement('div');
+        	qrReaderDiv.id = 'my-qr-reader';
+        	qrReaderContainer.appendChild(qrReaderDiv);
+        	qrReaderContainer.style.display = 'block';
+    		// Se si trova un QR code
+    		function onScanSuccess(decodeText, decodeResult) {
+       			alert("Codice trovato: " + decodeText);
+       			addPacked(decodeText);
+		}
+
+    		let htmlscanner = new Html5QrcodeScanner(
+		        "my-qr-reader",
+        		{ fps: 10, qrbox: 250 }
+    		);
+    		htmlscanner.render(onScanSuccess);
+	});
+}
+
+function addPacked(barcode) {
+    const expire = document.getElementById('newPackedExpire').value;
+    const quantity = document.getElementById('newPackedQuantity').value;
+    const isFridge = document.getElementById('newPackedFridge').checked;
+
+    if (!quantity) {
+        alert("COMPILA TUTTO");
+        return;
+    }
+
+    const url = "http://" + host + ":8080/ingsw24/gateway/" + pantryId + "/foods/packed/"+barcode+"?isFridge="+isFridge+"&quantity="+quantity+"&expirationDate="+expire;
+    const credentials = sessionStorage.getItem("credentials");
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.withCredentials = true;
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+                console.log(`Food ${barcode} added successfully`);
+                // Ricaricare il corpo per aggiornare l'elenco degli alimenti
+                buildBody();
+            } else {
+                console.error('There was a problem with the add operation:', this.statusText);
+            }
+        }
+    };
+
+    xhttp.open("PUT", url, true);
+    xhttp.setRequestHeader("Authorization", "Basic " + credentials);
+
+    xhttp.send();
+
 }
